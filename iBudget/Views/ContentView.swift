d12c4631b25715fd24d67e3003f602873b7cb1ef
gavinmorrow <gavinfmorrow@gmail.com>
2022-07-02 18:08:23 -0400

    Update navigation bar title

--- iBudget/Views/ContentView.swift
+++ iBudget/Views/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
 	var body: some View {
 		NavigationView {
 			ListView()
-				.navigationTitle("Expenses")
+				.navigationTitle("iBudget")
 				.toolbar {
 					ToolbarItem(placement: .navigationBarLeading) {
 						EditButton()
