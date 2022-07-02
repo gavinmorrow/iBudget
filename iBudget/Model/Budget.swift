203d6874cfd2a0c24960adfbde7e82910581ced8
gavinmorrow <gavinfmorrow@gmail.com>
2022-07-02 16:58:07 -0400

    Create Budget.swift
    A class to represent a budget

---
+++ iBudget/Model/Budget.swift
@@ -0,0 +1,41 @@
+//
+//  Budget.swift
+//  iBudget
+//
+//  Created by Gavin Morrow on 7/2/22.
+//
+
+import Foundation
+
+class Budget {
+	enum BudgetInterval {
+		case dailyx
+		case weekly
+		case monthly
+		case yearly
+		case custom(interval: TimeInterval)
+		case none
+	}
+	
+	init(amount: Double, timeSpan: BudgetInterval = .weekly, amountSpent: Double = 0) {
+		self.amount = amount
+		self.amountSpent = amountSpent
+		self.amountLeft = amount - amountSpent
+		
+		self.timeSpan = timeSpan
+	}
+	
+	var amount: Double
+	private(set) var amountSpent: Double
+	private(set) var amountLeft: Double
+	
+	var timeSpan: BudgetInterval = .weekly
+	
+	func spend(_ amount: Double) {
+		amountSpent += amount
+		amountLeft -= amount
+	}
+	func gain(_ amount: Double) {
+		spend(-amount)
+	}
+}
