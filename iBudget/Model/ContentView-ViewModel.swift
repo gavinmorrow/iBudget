7703180c2366cb40adf205c118039a3e32e1e6b6
gavinmorrow <gavinfmorrow@gmail.com>
2022-07-02 16:58:37 -0400

    Add support for budgeting

--- iBudget/Model/ContentView-ViewModel.swift
+++ iBudget/Model/ContentView-ViewModel.swift
@@ -9,24 +9,29 @@ import Foundation
 
 extension ContentView {
 	@MainActor class ViewModel: ObservableObject {
+		init() {
+			do {
+				guard let data: [Transaction] = try? FileManager().read(from: transactionsSavePath) else {
+					self.transactions = []
+					
+					struct ExitScope: Error {}
+					throw ExitScope()
+				}
+				
+				self.transactions = data
+			} catch {}
+			
+			self.budget = Budget(amount: 30)
+		}
 		
 		// MARK: Transactions
 		@Published private(set) var transactions: [Transaction]
 		
 		/// The path the transactions data is saved at.
-		let transactionsSavePath = FileManager.documentsDirectory
+		let transactionsSavePath: URL = FileManager().documentsDirectory
 			.appendingPathComponent("transactions.json")
 		
-		init() {
-			guard let data: [Transaction] = try? FileManager().read(from: transactionsSavePath) else {
-				transactions = []
-				return
-			}
-			
-			transactions = data
-		}
-		
-		/// Save the transactions array to disk
+		/// Save data to disk
 		func save() {
 			// Write with encryption b/c transactions should be private.
 			try? FileManager().write(
@@ -40,13 +45,26 @@ extension ContentView {
 		///
 		/// Adds a transaction to the beginning of the transactions array.
 		func add(transaction: Transaction) {
+			// Update budget
+			budget.spend(transaction.amount)
+			
+			// Add it to the list
 			transactions.insert(transaction, at: 0)
+			
 			save()
 		}
 		
 		/// Remove transactions from the transactions array.
 		func remove(at offsets: IndexSet) {
+			// Update budget
+			offsets.forEach { index in
+				let transaction = transactions[index]
+				budget.gain(transaction.amount)
+			}
+			
+			// Remove it from the list
 			transactions.remove(atOffsets: offsets)
+			
 			save()
 		}
 		
@@ -59,6 +77,11 @@ extension ContentView {
 		func transactions(in range: Range<Date>) -> [Transaction] {
 			transactions.filter { range.contains($0.date) }
 		}
+		
+		
+		// MARK: Budget
+		var budget: Budget
+		
 		
 		// MARK: State
 		@Published var showingSheet = false
