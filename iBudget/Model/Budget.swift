be29bde18fccaa6852fcf96ca60f899616e40e12
gavinmorrow <gavinfmorrow@gmail.com>
2022-07-05 19:49:27 -0400

    Extract BudgetInterval into separate file

--- iBudget/Model/Budget.swift
+++ iBudget/Model/Budget.swift
@@ -8,21 +8,7 @@
 import Foundation
 
 @MainActor class Budget: ObservableObject {
-	enum BudgetInterval: String, Hashable, Identifiable, CaseIterable {
-		case none
-		case daily
-		case weekly
-		case monthly
-		case yearly
-		
-		// TODO: Implement custom
-		// Maybe make an optional property that is only active if it is custom?
-//		case custom(interval: TimeInterval)
-		
-		var id: Self { self }
-	}
-	
-	init(amount: Double, timeSpan: BudgetInterval = .weekly, amountSpent: Double = 0) {
+	init(amount: Double, timeSpan: RepeatingTimeInterval = .weekly, amountSpent: Double = 0) {
 		self.amount = amount
 		self.amountSpent = amountSpent
 		self.amountLeft = amount - amountSpent
@@ -39,53 +25,9 @@ import Foundation
 	@Published private(set) var amountSpent: Double
 	@Published private(set) var amountLeft: Double
 	
-	@Published var timeSpan: BudgetInterval = .weekly
+	@Published var timeSpan: RepeatingTimeInterval = .weekly
 	var timeSpanString: String {
-		switch timeSpan {
-			case .none:
-				return ""
-			case .daily:
-				return "daily"
-			case .weekly:
-				return "weekly"
-			case .monthly:
-				return "monthly"
-			case .yearly:
-				return "yearly"
-//			case .custom(var interval):
-//				// TODO: clean up this code
-//				
-//				func round(_ num: Double, places: Int = 2)-> Double {
-//					guard places > 0 else { return 0 }
-//					return (num * pow(10.0, Double(places))).rounded() / pow(10.0, Double(places))
-//				}
-//				
-//				// test for minutes
-//				guard interval / 60 >= 1 else { return "every \(round(interval)) second(s)"}
-//				interval /= 60
-//				
-//				// test for hours
-//				guard interval / 60 >= 1 else { return "every \(round(interval)) minute(s)"}
-//				interval /= 60
-//				
-//				// test for days
-//				guard interval / 24 >= 1 else { return "every \(round(interval)) hour(s)"}
-//				interval /= 24
-//				
-//				// test for weeks
-//				guard interval / 7 >= 1 else { return "every \(round(interval)) day(s)"}
-//				interval /= 7
-//				
-//				// test for ~months
-//				guard interval / 30 >= 1 else { return "every \(round(interval)) week(s)"}
-//				interval /= 30
-//				
-//				// test for years
-//				guard interval / 365 >= 1 else { return "every \(round(interval)) month(s)"}
-//				interval /= 365
-//				
-//				return "every \(interval) year(s)"
-		}
+		RepeatingTimeInterval.rawValue(for: timeSpan)
 	}
 	
 	func spend(_ amount: Double) {
