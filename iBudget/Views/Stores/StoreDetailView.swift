8136976a3b3406f2e23b6778cc33319633e049f8
gavinmorrow <gavinfmorrow@gmail.com>
2022-07-10 08:14:01 -0400

    Fix StoreDetailView previews

--- iBudget/Views/Stores/StoreDetailView.swift
+++ iBudget/Views/Stores/StoreDetailView.swift
@@ -26,8 +26,25 @@ struct StoreDetailView: View {
 	}
 }
 
-//struct StoreDetailView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        StoreDetailView()
-//    }
-//}
+struct StoreDetailView_Previews: PreviewProvider {
+	static var previews: some View {
+		let viewModel = ViewModel()
+		
+		viewModel.removeStores(at: IndexSet(0..<viewModel.stores.count))
+		
+		let store = viewModel.addStore(name: "Test Store", notes: "Just a test :)")
+		
+		for i in 1...5 {
+			viewModel.addTransaction(
+				amount: Double(i),
+				type: i % 2 == 1 ? .credit : .debt,
+				store: store,
+				notes: "amount: \(i)"
+			)
+		}
+		
+		return NavigationView {
+			StoreDetailView(store: store)
+		}
+	}
+}
