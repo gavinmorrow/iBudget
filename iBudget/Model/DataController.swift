8b75f7c5004b3fb7c332957e6ebd42132a885058
gavinmorrow <gavinfmorrow@gmail.com>
2022-07-16 18:52:20 -0400

    Fix load data functions

--- iBudget/Model/DataController.swift
+++ iBudget/Model/DataController.swift
@@ -32,32 +32,62 @@ class DataController {
 	init(name: String) {
 		persistenContainerName = name
 	}
+	
+	func loadPersistentContainer() {
+		let _ = persistentContainer
+	}
 }
 
 extension DataController {
 	/// A function to load generic data from CoreData.
-	/// - Parameter defaultValue: The default value for the data if it can't be found. Defaults to `nil`.
+	///
+	/// - Parameters:
+	///   + sortDescriptors: Sort descriptors for the request. Defaults to an empty array.
+	///   + predicate: A search predicate for the request. If `nil`, no predicate will be applied. Defaults to `nil`.
+	///   + defaultValue: The default value for the data if it can't be found. Defaults to `nil`.
+	///
+	/// - Returns: The requested data, or the default value if it can't be found.
+	///
 	/// - Precondition: There must be only one instance of the data. If not, the first one will be returned.
-	func loadData<T: NSManagedObject>(predicate: NSPredicate = NSPredicate(), defaultValue: T? = nil) -> T? {
-		let fetchedResults: [T] = loadDataArray(predicate: predicate)
+	func loadData<T: NSManagedObject>(
+		sortDescriptors: [NSSortDescriptor] = [],
+		predicate: NSPredicate? = nil,
+		defaultValue: T? = nil
+	) -> T? {
+		let fetchedResults: [T] = loadDataArray(
+			sortDescriptors: sortDescriptors,
+			predicate: predicate,
+			resultsLimit: 1
+		)
 		
 		guard fetchedResults.count > 0 else {
 			print("WARNING: No CoreData results were found.")
 			return defaultValue
 		}
 		
-		if fetchedResults.count != 1 {
-			print("WARNING: There is more than 1 CoreData fetched result. Only the first one will be returned.")
-		}
-		
 		return fetchedResults[0]
 	}
 	
 	/// A function to load a generic array of data from CoreData.
-	/// - Parameter defaultValue: The default value for the data if it can't be found. Defaults to an empty array of the type.
-	func loadDataArray<T: NSManagedObject>(predicate: NSPredicate = NSPredicate(), defaultValue: [T] = []) -> [T] {
-		let fetchRequest = NSFetchRequest<T>()
+	/// - Parameters:
+	///   + sortDescriptors: Sort descriptors for the request. Defaults to an empty array.
+	///   + predicate: A search predicate for the request. If `nil`, no predicate will be applied. Defaults to `nil`.
+	///   + resultsLimit: The amount of results to fetch. If `nil`, all the results will be returned. Defaults to `nil`.
+	///   + defaultValue: The default value for the data if it can't be found. Defaults to an empty array.
+	///
+	/// - Returns: The requested array of data, or the default value if it can't be found.
+	func loadDataArray<T: NSManagedObject>(
+		sortDescriptors: [NSSortDescriptor] = [],
+		predicate: NSPredicate? = nil,
+		resultsLimit: Int? = nil,
+		defaultValue: [T] = []
+	) -> [T] {
+		loadPersistentContainer()
+		
+		let fetchRequest = T.fetchRequest() as! NSFetchRequest<T>
+		fetchRequest.sortDescriptors = sortDescriptors
 		fetchRequest.predicate = predicate
+		if let resultsLimit = resultsLimit { fetchRequest.fetchLimit = resultsLimit }
 		
 		return (try? moc.fetch(fetchRequest)) ?? defaultValue
 	}
