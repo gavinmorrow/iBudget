3b3ffee4d6d238443ca74717d273238da276cb52
gavinmorrow <gavinfmorrow@gmail.com>
2022-07-16 14:26:03 -0400

    Update DataController.swift
    Add loading methods

--- iBudget/Model/DataController.swift
+++ iBudget/Model/DataController.swift
@@ -34,3 +34,31 @@ class DataController {
 	}
 }
 
+extension DataController {
+	/// A function to load generic data from CoreData.
+	/// - Parameter defaultValue: The default value for the data if it can't be found. Defaults to `nil`.
+	/// - Precondition: There must be only one instance of the data. If not, the first one will be returned.
+	func loadData<T: NSManagedObject>(predicate: NSPredicate = NSPredicate(), defaultValue: T? = nil) -> T? {
+		let fetchedResults: [T] = loadDataArray(predicate: predicate)
+		
+		guard fetchedResults.count > 0 else {
+			print("WARNING: No CoreData results were found.")
+			return defaultValue
+		}
+		
+		if fetchedResults.count != 1 {
+			print("WARNING: There is more than 1 CoreData fetched result. Only the first one will be returned.")
+		}
+		
+		return fetchedResults[0]
+	}
+	
+	/// A function to load a generic array of data from CoreData.
+	/// - Parameter defaultValue: The default value for the data if it can't be found. Defaults to an empty array of the type.
+	func loadDataArray<T: NSManagedObject>(predicate: NSPredicate = NSPredicate(), defaultValue: [T] = []) -> [T] {
+		let fetchRequest = NSFetchRequest<T>()
+		fetchRequest.predicate = predicate
+		
+		return (try? moc.fetch(fetchRequest)) ?? defaultValue
+	}
+}
