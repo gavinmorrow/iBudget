39cb2e9b62af4044ccaa5c683b3315d6351858e3
gavinmorrow <gavinfmorrow@gmail.com>
2022-07-16 14:29:37 -0400

    Update widget

--- BalanceWidget/BalanceWidget.swift
+++ BalanceWidget/BalanceWidget.swift
@@ -41,7 +41,38 @@ struct SimpleEntry: TimelineEntry {
     let configuration: ConfigurationIntent
 	
 	var transactions: [Transaction]
-	var balance: Double { transactions.reduce(4_242_424_242.42, +) }
+	var balance: Double { transactions.reduce(4_242_424.24, +) }
+	var localizedBalance: String {
+		let numbers = ["Quintillion", "Quadrillion", "Trillion", "Billion", "Million"]
+		
+		//           3 powers higher than quintillion
+		if balance > 1_000_000_000_000_000_000_000 {
+			// "Zillion" is a fake number, but fun to use
+			// If someone has this much money,
+			// they don't need to see the exact amount in the widget
+			return "\(balance.rounded(to: 6)) Zillion"
+		} else if balance < 1_000_000 {
+			return balance.formatted(.currency(code: Locale.current.currencyCode ?? "USD"))
+		}
+		
+		var tempBal = balance / 1_000_000
+		
+		/// An index of `numbers`.
+		var i: Int = 0
+		
+		//                       subtract 1 otherwise `i`
+		//                       would become `numbers.count`.
+		while tempBal > 1 && i < numbers.count - 1 {
+			tempBal /= 1_000
+			i += 1
+			print(tempBal, i)
+		}
+		
+		// Ensure that `i` isn't too high
+		if i < numbers.count { i = numbers.count - 1 }
+		
+		return "\(tempBal.rounded(to: 2)) \(numbers[i])"
+	}
 	
 	init(date: Date, configuration: ConfigurationIntent) {
 		// Init date & config
@@ -61,7 +92,7 @@ struct BalanceWidgetEntryView : View {
 				.font(.headline)
 				.foregroundColor(.secondary)
 			
-			Text(entry.balance, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+			Text(entry.localizedBalance)
 				.font(.largeTitle)
 				.lineLimit(3)
 				.minimumScaleFactor(0.5)
