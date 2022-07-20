a488ac68bfc22d0f18e880e648943517537c7b10
gavinmorrow <gavinfmorrow@gmail.com>
2022-07-20 18:52:26 -0400

    Update Widget timeline, indentation, and info

--- BalanceWidget/BalanceWidget.swift
+++ BalanceWidget/BalanceWidget.swift
@@ -11,34 +11,28 @@ import Intents
 import CoreData
 
 struct Provider: IntentTimelineProvider {
-    func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), configuration: ConfigurationIntent())
-    }
-
-    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), configuration: configuration)
-        completion(entry)
-    }
-
-    func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
-
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, configuration: configuration)
-            entries.append(entry)
-        }
-
-        let timeline = Timeline(entries: entries, policy: .atEnd)
-        completion(timeline)
-    }
+	func placeholder(in context: Context) -> SimpleEntry {
+		SimpleEntry(date: Date(), configuration: ConfigurationIntent())
+	}
+	
+	func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+		let entry = SimpleEntry(date: Date(), configuration: configuration)
+		completion(entry)
+	}
+	
+	func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+		let entries: [SimpleEntry] = [
+			SimpleEntry(date: Date(), configuration: configuration)
+		]
+		
+		let timeline = Timeline(entries: entries, policy: .never)
+		completion(timeline)
+	}
 }
 
 struct SimpleEntry: TimelineEntry {
-    let date: Date
-    let configuration: ConfigurationIntent
+	let date: Date
+	let configuration: ConfigurationIntent
 	
 	var transactions: [Transaction]
 	var balance: Double { transactions.reduce(4_242_424.24, +) }
@@ -84,9 +78,9 @@ struct SimpleEntry: TimelineEntry {
 }
 
 struct BalanceWidgetEntryView : View {
-    var entry: Provider.Entry
-
-    var body: some View {
+	var entry: Provider.Entry
+	
+	var body: some View {
 		VStack(alignment: .leading) {
 			Text("Balance:")
 				.font(.headline)
@@ -103,25 +97,25 @@ struct BalanceWidgetEntryView : View {
 		}
 		.frame(maxWidth: .infinity, alignment: .leading)
 		.padding()
-    }
+	}
 }
 
 @main
 struct BalanceWidget: Widget {
-    let kind: String = "BalanceWidget"
-
-    var body: some WidgetConfiguration {
-        IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
-            BalanceWidgetEntryView(entry: entry)
-        }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
-    }
+	let kind: String = "BalanceWidget"
+	
+	var body: some WidgetConfiguration {
+		IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
+			BalanceWidgetEntryView(entry: entry)
+		}
+		.configurationDisplayName("Balance")
+		.description("View your current account balance.")
+	}
 }
 
 struct BalanceWidget_Previews: PreviewProvider {
-    static var previews: some View {
-        BalanceWidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
-            .previewContext(WidgetPreviewContext(family: .systemSmall))
-    }
+	static var previews: some View {
+		BalanceWidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
+			.previewContext(WidgetPreviewContext(family: .systemSmall))
+	}
 }
