23556f1454a4942c51a25ad231a346bb5c86ae4e
gavinmorrow <gavinfmorrow@gmail.com>
2022-07-21 15:23:52 -0400

    Create WidgetsController.swift
    A central place to manage reloading app widgets

---
+++ iBudget/Model/WidgetsController.swift
@@ -0,0 +1,28 @@
+//
+//  WidgetsController.swift
+//  iBudget
+//
+//  Created by Gavin Morrow on 7/21/22.
+//
+
+import class WidgetKit.WidgetCenter
+
+enum WidgetsController {
+	enum WidgetKind: String {
+		case balance = "BalanceWidget"
+	}
+	
+	static func updateAllWidgets() {
+		dPrint("Reloading all widgets.")
+		WidgetCenter.shared.reloadAllTimelines()
+	}
+	
+	static func updateWidget(ofKind kind: String) {
+		dPrint("Reloading widget of kind com.gm.iBudget.\(kind)")
+		WidgetCenter.shared.reloadTimelines(ofKind: kind)
+	}
+	
+	static func updateWidget(ofKind kind: WidgetKind) {
+		updateWidget(ofKind: kind.rawValue)
+	}
+}
